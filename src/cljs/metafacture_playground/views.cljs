9bc8761a7c75617bb2d49e01590030de58a6bf7a
katauber <katinka.tauber@hbz-nrw.de>
2021-01-29 14:44:58 +0100

    Improve styling.
    See https://github.com/metafacture/metafacture-playground/issues/1

--- src/cljs/metafacture_playground/views.cljs
+++ src/cljs/metafacture_playground/views.cljs
@@ -6,38 +6,47 @@
 
 (defn control-panel []
   [:div.row
-   [:input {:type "button"
-            :value "Load sample"
-            :on-click #(re-frame/dispatch [:load-sample])}]
-   [:input {:type "button"
-            :value "Clear all"
-            :on-click #(re-frame/dispatch [:clear-all])}]])
+   [:div.col
+    [:input.simple-button {:type "button"
+                           :value "Load sample"
+                           :on-click #(re-frame/dispatch [:load-sample])}]
+    [:input.simple-button {:type "button"
+                           :value "Clear all"
+                           :on-click #(re-frame/dispatch [:clear-all])}]]])
 
 (defn field-row [field-name]
-  (let [field-value(re-frame/subscribe [::subs/field-value [:fields field-name]])]
+  (let [field-value (re-frame/subscribe [::subs/field-value [:fields field-name]])]
     (fn [field-name]
-      [:div.row (str (-> field-name name clj-str/capitalize) ": ")
-       [:input {:type "text"
-                :id (name field-name)
-                :value @field-value
-                :on-change #(re-frame/dispatch [:edit [:fields field-name] (-> % .-target .-value)])}]])))
+      [:div.row
+       [:div.col-1
+        [:p (str (-> field-name name clj-str/capitalize) ": ")]]
+       [:div.col
+        [:input.simple-input {:type "text"
+                              :id (name field-name)
+                              :value @field-value
+                              :on-change #(re-frame/dispatch [:edit [:fields field-name] (-> % .-target .-value)])}]]])))
 
 (defn editor []
   (let [fix  (re-frame/subscribe [::subs/field-value [:fields :fix]])]
     (fn []
-      [:div.col "Fix:"
-       [:textarea {:id "fix"
+      [:div.col
+       [:textarea {:id "editor"
                    :value @fix
+                   :rows 25
+                   :cols 40
                    :on-change #(re-frame/dispatch [:edit [:fields :fix] (-> % .-target .-value)])}]])))
 
 (defn result-panel []
   [:div.col
-   [:input {:id "result"
-            :value "This could be your result"}]])
+   [:textarea {:id "result"
+               :rows 25
+               :cols 40}]])
 
 (defn main-panel []
   [:div.container
-   [:h1 "Metafacture Playground"]
+   [:div.row
+    [:div.col
+     [:h1.header  "Metafacture Playground"]]]
    [control-panel]
    [field-row :data]
    [field-row :flux]
