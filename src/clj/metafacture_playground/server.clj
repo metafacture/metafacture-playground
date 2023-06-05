c958031df2731d24a3237c715e9e97e167024986
katauber <katinka.tauber@hbz-nrw.de>
2023-06-05 09:18:36 +0200

    Add the Java Security Manager

--- src/clj/metafacture_playground/server.clj
+++ src/clj/metafacture_playground/server.clj
@@ -3,9 +3,12 @@
             [config.core :refer [env]]
             [clojure.tools.logging :as log]
             [ring.adapter.jetty :refer [run-jetty]])
+  (:import
+   (java.lang SecurityManager))
   (:gen-class))
-
+ 
  (defn -main [& _args]
    (let [port (or (env :port) 3000)]
+     (System/setSecurityManager (new SecurityManager))
      (log/info "Start server with port" port)
      (run-jetty #'handler {:port port :join? false :request-header-size 65536})))
