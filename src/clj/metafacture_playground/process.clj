90f0902ee491fa78f61052d09e27bb048bd9a443
katauber <katinka.tauber@hbz-nrw.de>
2024-01-12 08:57:25 +0100

    Delete files after successful processing. See #122

--- src/clj/metafacture_playground/process.clj
+++ src/clj/metafacture_playground/process.clj
@@ -1,7 +1,8 @@
 (ns metafacture-playground.process
   (:require
    [clojure.string :as clj-str]
-   [clojure.tools.logging :as log])
+   [clojure.tools.logging :as log]
+   [clojure.java.io :as io])
   (:import
    (java.io File)
    (org.metafacture.runner Flux)))
@@ -20,17 +21,28 @@
         (log/trace "Content" content)
         file-path))))
 
+(defn- delete-files! [file-pathes]
+  (doseq [file-path file-pathes]
+    (try
+      (io/delete-file file-path)
+      (log/info "Deleted file " file-path)
+      (catch Exception e
+        (log/warn "Could not delete file " file-path ". Catched exception: " (.getMessage e))))))
+
 
 (defn process [flux data transformation]
   (let [inputfile (content->tempfile-path data ".data")
-        transformationFile (content->tempfile-path transformation ".fix")
+        transformationfile (content->tempfile-path transformation ".fix")
         out-path (content->tempfile-path "" ".txt")
         output (str "|write(\"" out-path "\");")
         flux (-> (str "default inputFile = \"" inputfile "\";\n"
-                      "default transformationFile = \"" transformationFile "\";\n"
+                      "default transformationFile = \"" transformationfile "\";\n"
                       flux)
                  (clj-str/replace #"\|(\s*|\n*)write\(\".*\"\)(\s*|\n*);" output)
-                 (clj-str/replace #"\|(\s*|\n*)print(\s*|\n*);" output))]
-    (Flux/main (into-array [(content->tempfile-path flux ".flux")]))
+                 (clj-str/replace #"\|(\s*|\n*)print(\s*|\n*);" output))
+        fluxfile (content->tempfile-path flux ".flux")]
+    (Flux/main (into-array [fluxfile]))
     (log/info "Executed flux file with Flux/main. Result in" out-path)
-    (slurp out-path)))
+    (let [result (slurp out-path)]
+      (delete-files! [inputfile transformationfile fluxfile out-path])
+      result)))
