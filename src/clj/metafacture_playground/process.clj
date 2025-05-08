4bf31e7722dca7dfd032490da5b07df94608f3b5
Phu Tu <phu.tu@hbz-nrw.de>
2025-05-08 13:58:30 +0200

    Throw exception when output file exceeds 1 GB
    Resolves #159

--- src/clj/metafacture_playground/process.clj
+++ src/clj/metafacture_playground/process.clj
@@ -1,6 +1,7 @@
 (ns metafacture-playground.process
   (:require
    [clojure.string :as clj-str]
+   [clojure.java.io :as io]
    [clojure.tools.logging :as log])
   (:import
    (java.io File)
@@ -26,11 +27,21 @@
         transformationFile (content->tempfile-path transformation ".fix")
         out-path (content->tempfile-path "" ".txt")
         output (str "|write(\"" out-path "\");")
-        flux (-> (str "default inputFile = \"" inputfile "\";\n"
-                      "default transformationFile = \"" transformationFile "\";\n"
-                      flux)
-                 (clj-str/replace #"\|(\s*|\n*)write\(\".*\"\)(\s*|\n*);" output)
-                 (clj-str/replace #"\|(\s*|\n*)print(\s*|\n*);" output))]
-    (Flux/main (into-array [(content->tempfile-path flux ".flux")]))
-    (log/info "Executed flux file with Flux/main. Result in" out-path)
-    (slurp out-path)))
+        flux-str (-> (str "default inputFile = \"" inputfile "\";\n"
+                          "default transformationFile = \"" transformationFile "\";\n"
+                          flux)
+                     ;; Ersetze vorhandene |write(...) oder |print durch unsere Ausgabeanweisung
+                     (clj-str/replace #"\|(\s*|\n*)write\(\".*\"\)(\s*|\n*);" output)
+                     (clj-str/replace #"\|(\s*|\n*)print(\s*|\n*);" output))
+        fluxfile (content->tempfile-path flux-str ".flux")
+        max-size-bytes (* 1024 1024 1024)] ;; 1 GB
+
+    (try
+      ;; Führe das Flux-Programm aus
+      (Flux/main (into-array [fluxfile]))
+      (let [outfile (io/file out-path)]
+        (if (> (.length outfile) max-size-bytes)
+          (throw (ex-info "Output file exceeds maximum allowed size (1 GB)" {:file out-path}))
+          (do
+            (log/info "Executed flux file with Flux/main. Result in" out-path)
+            (slurp outfile)))))))
